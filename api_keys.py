119215470b5fd62c1c8701e50e10ddcd40b44587
Molleigh <144710935+molleighH@users.noreply.github.com>
2023-10-27 12:51:04 -0400

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "45a610858d1588e80b789dbe20d7afab"
+weather_api_key = "e9b2cb44b951cd8a5867fc854c348e43"
 
 # Geoapify API Key
 geoapify_key = "8b217bd7f4e14e5e82dfaa264d51ec99"
